c9a9356b4bff06c9119591a814934bdb675123c4
Alexandre Manoel da Silva <alemanoelsilva@protonmail.com>
2021-05-11 21:41:15 -0300

    refactor: add customized error for missing param

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,3 +1,4 @@
+import { MissingParamError } from '../errors/missing-param-error'
 import { httpResponse, httpRequest } from '../protocols/http'
 
 export class SignUpController {
@@ -5,15 +6,20 @@ export class SignUpController {
     if (!httpRequest.body.name) {
       return {
         statusCode: 400,
-        body: new Error('missing param: name')
+        body: new MissingParamError('name')
       }
     }
 
     if (!httpRequest.body.email) {
       return {
         statusCode: 400,
-        body: new Error('missing param: email')
+        body: new MissingParamError('email')
       }
+    }
+
+    return {
+      statusCode: 200,
+      body: {}
     }
   }
 }
