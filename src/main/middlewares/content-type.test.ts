178c7e966c65b1936692308f7057654c0815a719
Alexandre Manoel da Silva <alemanoelsilva@protonmail.com>
2021-05-19 21:49:48 -0300

    feat: ensure requests return json as default content type

---
+++ src/main/middlewares/content-type.test.ts
@@ -0,0 +1,25 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('Content Type Middleware', () => {
+  test('Should return default content type as json', async () => {
+    app.get('/test_content_type', (q, r) => {
+      r.send()
+    })
+
+    await request(app)
+      .get('/test_content_type')
+      .expect('content-type', /json/)
+  })
+
+  test('Should return xml content type when forced', async () => {
+    app.get('/test_content_type_xml', (q, r) => {
+      r.type('xml')
+      r.send()
+    })
+
+    await request(app)
+      .get('/test_content_type_xml')
+      .expect('content-type', /xml/)
+  })
+})
