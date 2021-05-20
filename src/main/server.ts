57e71693dd7aa6035dbcc6757f6d97b01840c3f7
Alexandre Manoel da Silva <alemanoelsilva@protonmail.com>
2021-05-19 22:14:22 -0300

    feat: connect to mongodb before app initialization

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,10 @@
-import app from './config/app'
+import env from './config/env'
+import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
 
-app.listen(5050, () => console.log('Running'))
+MongoHelper.connect(env.mongoUrl)
+  .then(async () => {
+    const app = (await import('./config/app')).default
+
+    app.listen(env.port, () => console.log('Running'))
+  })
+  .catch(console.error)
