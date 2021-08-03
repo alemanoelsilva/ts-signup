1d8496b211d411e073e9722dd677a9577b08da5e
Alexandre Manoel da Silva <alexandrem.silva@toroinvestimentos.com.br>
2021-08-03 07:44:31 -0300

    feat: ensure LoginController calls EmailValidator with correct emai

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,8 +1,11 @@
 import { MissingParamError } from '../../errors'
 import { badRequest } from '../../helpers/http-helpers'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
+import { EmailValidator } from '../signup/signup-protocols'
 
 export class LoginController implements Controller {
+  constructor(private readonly emailValidator: EmailValidator) {}
+
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     if (!httpRequest.body.email) {
       return new Promise((resolve) =>
@@ -15,6 +18,8 @@ export class LoginController implements Controller {
         resolve(badRequest(new MissingParamError('password')))
       )
     }
+
+    this.emailValidator.isValid(httpRequest.body.email)
 
     return new Promise((resolve) =>
       resolve({
