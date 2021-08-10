58e07ae4e91410b363f0dd22ca893e8a2b1fc3e8
Alexandre Manoel da Silva <alexandrem.silva@toroinvestimentos.com.br>
2021-08-10 07:55:13 -0300

    test: ensure ValidationComposite returns an error if any validation fails

---
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -0,0 +1,20 @@
+import { MissingParamError } from '../../errors'
+import { Validation } from './validation'
+import { ValidationComposite } from './validation-composite'
+
+describe('Validation Composite', () => {
+  test('Should return an error if any validation fails', () => {
+    class ValidationStub implements Validation {
+      validate(input: any): Error {
+        return new MissingParamError('field')
+      }
+    }
+
+    const validationsStub = new ValidationStub()
+
+    const sut = new ValidationComposite([validationsStub])
+    const error = sut.validate({ field: 'any_value' })
+
+    expect(error).toEqual(new MissingParamError('field'))
+  })
+})
