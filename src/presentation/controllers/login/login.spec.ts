96b59b4ba7a27fb49eaaa741d0839f23c747332c
Alexandre Manoel da Silva <alexandrem.silva@toroinvestimentos.com.br>
2021-08-13 19:07:51 -0300

    refactor: change Authentication to receive a model

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -8,10 +8,11 @@ import {
 import { HttpRequest, Authentication } from './login-protocols'
 import { LoginController } from './login'
 import { Validation, VALIDATION_TYPE } from '../signup/signup-protocols'
+import { AuthenticationModel } from '../../../domain/usecases/authentication'
 
 const makeAuthentication = (): Authentication => {
   class AuthenticationStub implements Authentication {
-    async auth(email: string, password: string): Promise<string> {
+    async auth(authentication: AuthenticationModel): Promise<string> {
       return new Promise((resolve) => resolve('any_token'))
     }
   }
@@ -63,7 +64,10 @@ describe('Login Controller', () => {
 
     await sut.handle(httpRequest)
 
-    expect(authSpy).toHaveBeenCalledWith('any_email', 'any_password')
+    expect(authSpy).toHaveBeenCalledWith({
+      email: 'any_email',
+      password: 'any_password'
+    })
   })
 
   test('should return 401 if invalid credentials are provided', async () => {
