5846d1380bfa577097fcec2cab1f78578e0bebe8
Alexandre Manoel da Silva <alexandrem.silva@toroinvestimentos.com.br>
2021-08-14 18:37:45 -0300

    refactor: add HashComparer interface on BcryptAdapter

--- src/infra/criptography/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt-adapter.ts
@@ -1,7 +1,8 @@
 import bcrypt from 'bcrypt'
+import { HashComparer } from '../../data/protocols/criptography/hash-comparer'
 import { Hasher } from '../../data/protocols/criptography/hasher'
 
-export class BcryptAdapter implements Hasher {
+export class BcryptAdapter implements Hasher, HashComparer {
   constructor(private readonly salt: number) {}
 
   async hash(value: string): Promise<string> {
